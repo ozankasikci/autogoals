8e03f3b651c6541eadaa4968711294dedb720189
Ozan Kaşıkçı <ozan@kasikci.io>
2026-03-18 23:11:25 +0300

    set dashboard dev server port to 5891

--- packages/dashboard/vite.config.ts
+++ packages/dashboard/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5891,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
